1e21ba37d88b5f9a1ebbcc1a7b13c9c8e10b7277
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-03-16 22:25:04 +0300

    chore: added some comments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//imports
 const getReversedString = require('./utils/get-reverse-string');
 
 process.stdout.write('Input any string for reversing: ');
 
+//input string and output reversed string
 process.stdin.on('data', (data) => {
   let reversedString = getReversedString(data.toString());
 
