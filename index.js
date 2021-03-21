e12b90dd017e12f9d85fa8dfe5862bfc594fe495
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-03-21 23:18:21 +0300

    feat: added simple get and post request

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3000
+/* eslint-disable consistent-return */
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.LOCAL_HOST;
+
+const db = {
+  users: [
+    {
+      login: 'Alex',
+      password: 'qwerty',
+      age: '23',
+      isDeleted: false,
+    },
+  ],
+};
+
+app.use('/users', bodyParser.json());
+
 app.get('/', (request, response) => {
-    response.send('Hello from Express!')
-})
+  response.send('Hello from Express!');
+});
+
+app.get('/users', (request, response) => {
+  response.send(JSON.stringify(db.users));
+});
+
+app.post('/users', (req, res, next) => {
+  console.log(req.body);
+});
+
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port} click link \x1b[36m http://localhost:3000 \x1b[0m `)
-})
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+  console.log(`server is listening on ${port} click link \x1b[36m http://localhost:3000 \x1b[0m `);
+});
