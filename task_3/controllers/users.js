e1c30735e16d3f658a6c80816db65992ef11f189
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-04-04 16:06:43 +0300

    feat: add try catch

--- task_3/controllers/users.js
+++ task_3/controllers/users.js
@@ -9,57 +9,99 @@ import {
 } from '../services/users';
 
 const getMainPageHandler = (req, res) => {
-  const message = mainPage();
-  res.status(200);
-  res.send(message);
+  try {
+    const message = mainPage();
+
+    res.status(200);
+    res.send(message);
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
+  }
 };
 
 const getUsersHandler = (req, res) => {
   const { loginSubstring, limit } = req.query;
 
-  if (loginSubstring && limit) {
-    const limetedUsersCollection = getSortAndLimitUsers(loginSubstring, limit);
-    res.status(200);
-    res.send(JSON.stringify(limetedUsersCollection));
-  } else {
-    const users = getUsers();
-    res.status(200);
-    res.send(JSON.stringify(users));
+  try {
+    if (loginSubstring && limit) {
+      const limetedUsersCollection = getSortAndLimitUsers(loginSubstring, limit);
+
+      res.status(200);
+      res.send(JSON.stringify(limetedUsersCollection));
+    } else {
+      const users = getUsers();
+      res.status(200);
+      res.send(JSON.stringify(users));
+    }
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
   }
 };
 
 const getUserByIdHandler = (req, res) => {
-  const { id } = req.params;
-  const userInfo = findUserById(id);
+  try {
+    const { id } = req.params;
+    const userInfo = findUserById(id);
 
-  res.status(200);
-  res.send(JSON.stringify(userInfo));
+    res.status(200);
+    res.send(JSON.stringify(userInfo));
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
+  }
 };
 
 const createNewUserHandler = (req, res) => {
-  console.log(res.body);
-  const users = pushNewUser(req.body);
+  try {
+    const users = pushNewUser(req.body);
 
-  res.status(200);
-  res.send(JSON.stringify(users));
+    res.status(200);
+    res.send(JSON.stringify(users));
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
+  }
 };
 
 const updateUserHandler = (req, res) => {
-  console.log(req.body);
-  const { id } = req.params;
-  const { login, password, age } = req.body;
+  try {
+    const { id } = req.params;
+    const { login, password, age } = req.body;
+    const users = updateUserInDatabase(id, login, password, age);
 
-  const users = updateUserInDatabase(id, login, password, age);
-  res.status(200);
-  res.send(JSON.stringify(users));
+    res.status(200);
+    res.send(JSON.stringify(users));
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
+  }
 };
 
 const deleteUserHandler = (req, res) => {
-  const { id } = req.params;
-  const updatedUsers = setDeletedUser(id);
+  try {
+    const { id } = req.params;
+    const updatedUsers = setDeletedUser(id);
 
-  res.status(200);
-  res.send(JSON.stringify(updatedUsers));
+    res.status(200);
+    res.send(JSON.stringify(updatedUsers));
+  } catch (err) {
+    if (!err.statusCode) {
+      res.status(500);
+      res.send(JSON.stringify(JSON.stringify({ message: 'Error' })));
+    }
+  }
 };
 
 export {
