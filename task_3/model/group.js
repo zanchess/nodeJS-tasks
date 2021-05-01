30e5bed62c3b94d6064f1fc12623b8de81a776f3
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-05-01 13:15:48 +0300

    feat: added group

---
+++ task_3/model/group.js
@@ -0,0 +1,35 @@
+import {Sequelize, DataTypes} from 'sequelize';
+import sequelize from '../data-access/db';
+
+const PERMISSION_TYPES = {
+  READ: 'READ',
+  WRITE: 'WRITE',
+  DELETE: 'DELETE',
+  SHARE: 'SHARE',
+  UPLOAD_SHARE: 'UPLOAD_SHARE'
+};
+
+const Group = sequelize.define('Group', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  permissions: {
+    type: DataTypes.ENUM(Object.values(PERMISSION_TYPES)),
+    allowNull: false,
+    defaultValue: PERMISSION_TYPES.READ
+  }
+}, {
+  tableName: 'group',
+  createdAt: false,
+  updatedAt: false
+});
+
+export default Group;
+
