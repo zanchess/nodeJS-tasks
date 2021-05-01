fb1e67b15cda3d37002bee5984fe9a93b2e97a4e
Aliaksandr Lashkevich <zanchesssss@gmail.com>
2021-05-01 17:06:28 +0300

    feat: added userGroup in model

---
+++ task_3/model/userGroup.js
@@ -0,0 +1,10 @@
+import Users from './users';
+import Groups from './group';
+
+Users.belongsToMany(Groups, {
+  through: 'UserGroups'
+});
+
+Groups.belongsToMany(Users, {
+  through: 'UserGroups'
+});
